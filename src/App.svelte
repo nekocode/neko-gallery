8a2fd97b461a37accc72d6b46e934e9588f41aef
nekocode <nekocode.cn@gmail.com>
2021-01-29 20:46:52 +0800

    Uses data from real api & added loading component

--- src/App.svelte
+++ src/App.svelte
@@ -3,36 +3,44 @@ import { onMount } from "svelte";
 import { Octokit } from "@octokit/rest";
 import axios from "axios";
 import type { User, Repo, LangColor } from "./utils/types";
+import Loading from "./components/Loading.svelte";
 import Header from "./components/Header.svelte";
 import RepoItem from "./components/RepoItem.svelte";
 import { fakeRepos, fakeUser } from "./fake_data";
 
+const username = "nekocode";
 const octokit = new Octokit({
   previews: ["mercy-preview"],
 });
+
 let user: User;
 let repos: Repo[] = [];
 let langColors: LangColor;
 let selectedTopic: string = window.unescape(window.location.hash.substr(1));
+let loading = true;
 
 const onTopicSelect = (topic: string) => {
   selectedTopic = topic;
 };
 
-onMount(() => {
-  axios
-    .get("./lang_colors.json")
-    .then(function (response) {
-      langColors = response.data;
-      user = fakeUser;
-      repos = fakeRepos;
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+onMount(async () => {
+  const res1 = await axios.get("./lang_colors.json");
+  const res2 = await octokit.users.getByUsername({
+    username,
+  });
+  const res3 = await octokit.repos.listForUser({
+    username,
+    per_page: 200,
+  });
+
+  langColors = res1.data;
+  user = res2.data;
+  repos = res3.data;
+  loading = false;
 });
 </script>
 
+<Loading hidden="{!loading}" />
 <main>
   {#if user}
     <Header
---
+++ src/components/Loading.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+export let hidden: boolean = false;
+</script>
+
+<div id="splash" class="{hidden ? ['hidden'] : null}">
+  <div class="content">
+    <div class="lds-ripple">
+      <div></div>
+      <div></div>
+    </div>
+    <div class="text-hint">Gallery is now loading...</div>
+  </div>
+</div>
+
+<style lang="scss">
+#splash {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(45deg, #222428, #444856);
+  color: #fff;
+  z-index: 4000;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.3s linear;
+}
+:global(#splash.hidden) {
+  opacity: 0;
+  z-index: -1;
+  visibility: hidden;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.text-hint {
+  margin-top: 10px;
+  color: #6c757d;
+}
+
+.lds-ripple {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-ripple div {
+  position: absolute;
+  border: 4px solid #fff;
+  opacity: 1;
+  border-radius: 50%;
+  animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+}
+.lds-ripple div:nth-child(2) {
+  animation-delay: -0.5s;
+}
+@keyframes lds-ripple {
+  0% {
+    top: 36px;
+    left: 36px;
+    width: 0;
+    height: 0;
+    opacity: 1;
+  }
+  100% {
+    top: 0px;
+    left: 0px;
+    width: 72px;
+    height: 72px;
+    opacity: 0;
+  }
+}
+</style>
